4cbe701d7da5e28dc4e4e741d0b9f0826c51df09
Jhony Alexander Garcia Gómez <jhonyagarcia@gmail.com>
2020-05-20 14:38:57 -0500

    service worker

--- assets/index.js
+++ assets/index.js
@@ -30,3 +30,9 @@ function updateControls(){
 
 
 updateControls();
+
+if ("serviceWorker" in navigator){
+    navigator.serviceWorker.register('/sw.js').catch(error => {
+        console.log(error.message);
+    });
+}
